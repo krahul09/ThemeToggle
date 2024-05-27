30fef3bce22c274b9ef1e235ceda106f296b8e02
krahul09 <kumar.rahul012250@gmail.com>
2024-05-27 06:58:37 +0530

    Created a card light mode to dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/Card";
+import ThemeButton from "./components/ThemeButton";
+import { ThemeProvider } from "./contexts/themeContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeProvider>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black">
+      <div className="mb-5">
+        <ThemeButton/>
+        </div>
+      <Card/>
     </div>
+  </ThemeProvider>
   );
 }
 
